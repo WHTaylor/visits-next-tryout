cdac52fb84a6cff36057455501f4491bc51817ec
Will Taylor <william.h.taylor@stfc.ac.uk>
2023-07-14 17:14:05 +0100

    Handle 401s on /visit/[id]

--- src/app/visit/[id]/page.tsx
+++ src/app/visit/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { deserialize, Visit } from "@/types";
+import { Box, Link, Paper } from "@mui/material";
+import styles from "./page.module.css";
 
 type Props = {
   params: Params;
@@ -9,19 +11,22 @@ type Params = {
 };
 
 const ViewVisit = async ({ params }: Props) => {
-  const visit: Visit | "oh no" = await fetch(
+  const visit: Visit | "error" = await fetch(
     "http://localhost:3000/api/visits/" + params.id,
   ).then(async (res) =>
-    res.status === 404
-      ? "oh no"
+    res.status !== 200
+      ? "error"
       : ((await res.json().then((j) => deserialize(j.visit))) as Visit),
   );
 
-  return visit === "oh no" ? (
-    <p>
-      That visit doesn&apos;t exist, and I don&apos;t know how to do error
-      handling properly
-    </p>
+  return visit === "error" ? (
+    <Box
+      className={"centered-column-flex " + styles.errorContainer}
+      component={Paper}
+    >
+      <p>Visit {params.id} is unavailable.</p>
+      <Link href="/">Go back</Link>
+    </Box>
   ) : (
     <p>This is visit {visit.id}</p>
   );
